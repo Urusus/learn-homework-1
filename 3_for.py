084963001b471837bdcfe2c5b0dcd80df861d00f
Sergey Urusov <urusovsergey@gmail.com>
2022-05-31 17:42:01 +0200

    third task completed

--- 3_for.py
+++ 3_for.py
@@ -16,28 +16,29 @@
 * Посчитать и вывести среднее количество продаж всех товаров
 """
 
-
 phones = [
   {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]}, 
-    {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316]},
-    {'product': 'Samsung Galaxy 21', 'items_sold': [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247]},
+  {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316]},
+  {'product': 'Samsung Galaxy 21', 'items_sold': [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247]},
 ]
 
-total_sales = 0
-total_sales_avg = 0
+Iphone_total_sales = sum(phones[0]["items_sold"])
+Xiaomi_total_sales = sum(phones[1]["items_sold"])
+Samsung_total_sales = sum(phones[2]["items_sold"])
 
-for sales_per_model in phones:
+Iphone_average_sales = sum(phones[0]["items_sold"]) / len(phones[0]["items_sold"])
+Xiaomi_average_sales = sum(phones[1]["items_sold"]) / len(phones[1]["items_sold"])
+Samsung_average_sales = sum(phones[2]["items_sold"]) / len(phones[2]["items_sold"])
 
-  model_sales = sum(sales_per_model["items_sold"])
-  print(f'Общее количество продаж {sales_per_model["product"]}: {model_sales}')
+all_model_sales = phones[0]["items_sold"] + phones[1]["items_sold"] + phones[2]["items_sold"]
 
-  avg_model_sales = sum(sales_per_model['items_sold']) / len(sales_per_model['items_sold'])
-  print(f'Среднее количестыо продаж {sales_per_model["product"]}: {avg_model_sales}')
-  
-  total_sales += model_sales
-  total_sales_avg += avg_model_sales / len(phones)
-
+print("Общее количество продаж", phones[0]["product"], "-", Iphone_total_sales)
+print("Общее количество продаж", phones[1]["product"], "-", Xiaomi_total_sales)
+print("Общее количество продаж", phones[2]["product"], "-", Samsung_total_sales)
 print()
-print(f'Общее количество продаж: {total_sales}')
-
-print(f'Общее среднее количество продаж: {total_sales_avg}')
+print("Среднее количество продаж", phones[0]["product"], "-", Iphone_average_sales)
+print("Среднее количество продаж", phones[1]["product"], "-", Xiaomi_average_sales)
+print("Среднее количество продаж", phones[2]["product"], "-", Samsung_average_sales)
+print()
+print("Cуммарное количество продаж всех товаров", "-", sum(all_model_sales))
+print("Среднее количество продаж всех товаров", "-", sum(all_model_sales) / len(all_model_sales))
