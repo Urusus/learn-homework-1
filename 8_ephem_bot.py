8bae6e311d8010f09995fe654fde11e8c1262042
Sergey Urusov <urusovsergey@gmail.com>
2022-06-06 11:58:46 +0200

    Задание 8 выполнено для Марса

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -12,7 +12,44 @@
   бота отвечать, в каком созвездии сегодня находится планета.
 
 """
+
+import logging
+from pydoc import plain
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+logging.basicConfig(filename="bot.log", level=logging.INFO)
+import Settings
 import ephem
 
-planet = input()
+def greet_user(update, context):
+    print("Вызван /start")
+    print(update)
+    update.message.reply_text("Здравствуй, пользователь")
+
+def planet_info(update, context):
+    print("Planet provided:")
+    planet = update.message.text.split()[1]
+    print(planet)
+    
+    if planet == "Mars":
+      Constellation = ephem.constellation(ephem.Mars('2022/06/06'))
+      print(Constellation)
+      update.message.reply_text('Today Mars is this constellation:')
+      update.message.reply_text(str(Constellation))
+    else:
+      print("Mars not selected")
+      update.message.reply_text('Please write: /planet Mars')
+
+def main():
+    mybot = Updater(Settings.TG_API_KEY, use_context=True)
+
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", planet_info))
+    
+    mybot.start_polling()
+    mybot.idle()
+
+if __name__ == "__main__":
+    main()
+
 
